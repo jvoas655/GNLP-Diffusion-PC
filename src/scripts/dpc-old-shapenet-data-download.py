e2d81cd601dc21d6ebae5c62b448a8b18a883448
Zayne-sprague <zayne_sprague@yahoo.com>
2022-04-07 00:27:53 -0500

    Refactor issue

--- src/scripts/dpc-old-shapenet-data-download.py
+++ src/scripts/dpc-old-shapenet-data-download.py
@@ -20,17 +20,12 @@ if __name__ == "__main__":
 
     args = argparser.parse_args()
 
-    output_path: Path = Path(args.output) / 'shapenetcorev2_hdf5_2048.zip'
-    output_path2: Path = Path(args.output) / 'shapenet.hdf5'
+    output_path: Path = Path(args.output) / 'shapenet.hdf5'
 
     force: bool = args.force
     quiet: bool = args.quiet
 
     assert not output_path.exists() or force, f"Something already exists at {output_path}, pick a new place or use -f."
 
-    data_file_id = '15eG9fdwhBvroPg_NxBQ-1l4_Q1A-_9KC'
+    data_file_id = '1WdooIyOMVt_HVd_A0QOM9DTTX41O65jQ'
     gdown.download(output=str(output_path), quiet=quiet, id=data_file_id)
-
-    with zipfile.ZipFile(Path(args.output) / 'shapenetcorev2_hdf5_2048.zip', 'r') as zip_ref:
-        zip_ref.extractall(Path(args.output))
-    os.remove(Path(args.output) / 'shapenetcorev2_hdf5_2048.zip')
